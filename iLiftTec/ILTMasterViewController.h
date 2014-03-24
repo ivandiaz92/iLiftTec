a8247e61d5246a0d4c09b8c41a04698892089b44
Ivan Diaz <ivandiaz@ivans-mbp-3.lan>
2014-03-23 19:51:18 -0600

    Initial Commit

---
+++ iLiftTec/ILTMasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  ILTMasterViewController.h
+//  iLiftTec
+//
+//  Created by Ivan Diaz on 3/23/14.
+//  Copyright (c) 2014 Ivan Diaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ILTMasterViewController : UITableViewController
+
+@end
